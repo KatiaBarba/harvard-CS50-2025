3df519339dd2a0fe2f94ccce8979d39cd63f613c
KatiaBarba <katiabarba5@hotmail.com>
2025-02-18 14:13:22 -0600

    elimina cash mio y crea cash con el paso a paso de CS50

--- week-1/cash/cash.c
+++ week-1/cash/cash.c
@@ -6,38 +6,46 @@
 #define NICKLE 5
 #define PENNY 1
 
-// Prototype of functions
-int calculate_coins(int dividend, int divisor);
+// Prototype functions
 int get_cents();
+int calculate_coins(int dividend, int divisor);
 
 int main(void)
 {
-    // Declaration of variables
-    int total_coins = 0; // Initialize a variable to sum the number of coins of each denomination
-    int cents = get_cents(); // ask the amount in cents
+    // Prompt the user for change owed, in cents
+    int cents = get_cents();
 
-    // Calculate quarters using the function and add the return value to the varable
-    total_coins += calculate_coins(cents, QUARTER);
+    // Calculate how many quarters you should give customer
+    int quarters = calculate_coins(cents, QUARTER);
 
-    // Se actualiza la variable cents
-    cents = cents % QUARTER;
+    // Subtract the value of those quarters from cents
+    cents -= (quarters * QUARTER);
 
-    // Calculate dimes (repite the process for each denomination)
-    total_coins += calculate_coins(cents, DIME);
+    // Calculate how many dimes you should give customer
+    int dimes = calculate_coins(cents, DIME);
 
-    cents = cents % DIME;
+    // Subtract the value of those dimes from remaining cents
+    cents %= DIME;
 
-    // Calculate nickles
-    total_coins += calculate_coins(cents, NICKLE);
+    // Calculate how many nickels you should give customer
+    int nickles = calculate_coins(cents, NICKLE);
 
+    // Subtract the value of those nickels from remaining cents
     cents %= NICKLE;
 
-    total_coins += cents; // Add the last coins to de variable "total_coins"
+    // Calculate how many pennies you should give customer
+    int pennies = calculate_coins(cents, PENNY);
 
-    printf("%i\n", total_coins);
+    // Subtract the value of those pennies from remaining cents;
+    cents = cents - (pennies * PENNY); // cents equal 0
+
+    // Sum the number of quarters, dimes, nickels, and pennies used
+    int total_cents = quarters + dimes + nickles + pennies;
+
+    // Print that sum
+    printf("%i\n", total_cents);
 }
-// ---------------------FUNCTIONS-----------------------------------
-
+// ------------ FUNCTIONS -----------------------------------------
 // Prompt the user for change owed, in cents
 int get_cents()
 {
@@ -46,11 +54,11 @@ int get_cents()
     {
         cents = get_int("Change owed: ");
     }
-    while (cents <= 0);
+    while (cents < 0);
     return cents;
 }
 
-// -------Divisions for each denomination-------
+// Calculate how many quarters you should give customer
 int calculate_coins(int dividend, int divisor)
 {
     return dividend / divisor;
